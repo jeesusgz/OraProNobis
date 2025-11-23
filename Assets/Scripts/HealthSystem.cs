629a0a90df1890b39fda4d37926b48f51f1804c6
jeesusgz <115544256+jeesusgz@users.noreply.github.com>
2025-11-24 00:49:38 +0100

    animacion de daño y muerte (player)

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+ï»¿using UnityEngine;
 using System.Collections;
 
 public class HealthSystem : MonoBehaviour
@@ -6,47 +6,99 @@ public class HealthSystem : MonoBehaviour
     public int maxHealth = 3;
     private int currentHealth;
 
-    public float invulnerabilityTime = 1f; // Tiempo de invulnerabilidad
-    private bool isInvulnerable = false;   // Controla si puede recibir daño
+    public float invulnerabilityTime = 1f;
+    public float flashSpeed = 0.1f;
+
+    private bool isInvulnerable = false;
+    private bool isPlayer = false;
+    private bool isDying = false;
+
+    private Animator anim;
+    private SpriteRenderer spriteRenderer;
+    private MonoBehaviour[] scripts;
 
     void Start()
     {
         currentHealth = maxHealth;
+
+        anim = GetComponentInChildren<Animator>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+        isPlayer = CompareTag("Player");
+        scripts = GetComponents<MonoBehaviour>();
     }
 
     public void TakeDamage(int damageAmount)
     {
-        // Si está invulnerable, no recibe daño
-        if (isInvulnerable)
+        if (isInvulnerable || isDying)
             return;
 
         currentHealth -= damageAmount;
-        Debug.Log(gameObject.name + " ha recibido " + damageAmount + " de daño. Vida actual: " + currentHealth);
+        Debug.Log(gameObject.name + " ha recibido daÃ±o. Vida: " + currentHealth);
 
-        // Inicia invulnerabilidad
         StartCoroutine(InvulnerabilityCoroutine());
 
         if (currentHealth <= 0)
-        {
-            Die();
-        }
+            StartCoroutine(DieRoutine());
     }
 
     IEnumerator InvulnerabilityCoroutine()
     {
         isInvulnerable = true;
 
-        // Opcional: hacer parpadear al personaje
-        // TODO: agregar efecto visual si quieres
+        float timer = 0f;
 
-        yield return new WaitForSeconds(invulnerabilityTime);
+        while (timer < invulnerabilityTime)
+        {
+            if (spriteRenderer != null)
+            {
+                spriteRenderer.color = Color.red;
+                yield return new WaitForSeconds(flashSpeed);
+
+                spriteRenderer.color = Color.white;
+                yield return new WaitForSeconds(flashSpeed);
+            }
+
+            timer += flashSpeed * 2f;
+        }
+
+        if (spriteRenderer != null)
+            spriteRenderer.color = Color.white;
 
         isInvulnerable = false;
     }
 
+    IEnumerator DieRoutine()
+    {
+        isDying = true;
+
+        Debug.Log(gameObject.name + " estÃ¡ muriendo...");
+
+        if (isPlayer && anim != null)
+            anim.SetTrigger("Die");
+
+        foreach (var s in scripts)
+        {
+            if (s != this)
+                s.enabled = false;
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        Die();
+    }
+
     void Die()
     {
-        Debug.Log(gameObject.name + " ha muerto.");
-        Destroy(gameObject);
+        if (isPlayer)
+        {
+            Debug.Log("PLAYER MUERTO");
+            // TODO: respawn o pantalla de game over
+        }
+        else
+        {
+            Debug.Log(gameObject.name + " enemigo/paso destruido");
+            Destroy(gameObject);
+        }
     }
 }
