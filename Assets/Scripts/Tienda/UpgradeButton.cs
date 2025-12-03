3b7559a8c14cb91ed1d581a1f42cba5b1c7ea0c4
jeesusgz <115544256+jeesusgz@users.noreply.github.com>
2025-12-03 02:38:17 +0100

    crear sistema de compra

---
+++ Assets/Scripts/Tienda/UpgradeButton.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UpgradeButton : MonoBehaviour
+{
+    public int precio = 10;
+    public Button button;
+
+    public enum UpgradeType { Jugador, Paso, Nazarenos }
+    public UpgradeType tipoUpgrade;
+
+    public GameObject mensajePrefab; // Prefab de texto que se mostrará
+    public Transform canvasTransform; // Canvas donde se mostrará el mensaje
+
+    void Update()
+    {
+        if (CurrencyManager.Instance != null && button != null)
+        {
+            // Activar o desactivar según monedas
+            button.interactable = CurrencyManager.Instance.gameData.monedas >= precio;
+        }
+    }
+
+    public void ComprarMejora()
+    {
+        if (CurrencyManager.Instance.TrySpend(precio))
+        {
+            // Subir nivel según tipo
+            switch (tipoUpgrade)
+            {
+                case UpgradeType.Jugador:
+                    CurrencyManager.Instance.gameData.vidaJugadorNivel++;
+                    break;
+                case UpgradeType.Paso:
+                    CurrencyManager.Instance.gameData.vidaPasoNivel++;
+                    break;
+                case UpgradeType.Nazarenos:
+                    CurrencyManager.Instance.gameData.cantidadNazarenos++;
+                    if (CurrencyManager.Instance.gameData.cantidadNazarenos > 4)
+                        CurrencyManager.Instance.gameData.cantidadNazarenos = 4;
+                    break;
+            }
+
+            // Guardar
+            SaveSystem.Save(CurrencyManager.Instance.gameData);
+
+            // Mostrar mensaje
+            if (mensajePrefab != null && canvasTransform != null)
+            {
+                GameObject mensaje = Instantiate(mensajePrefab, canvasTransform);
+                mensaje.GetComponent<UnityEngine.UI.Text>().text = $"¡{tipoUpgrade} sube de nivel!";
+                StartCoroutine(DesaparecerMensaje(mensaje, 2f));
+            }
+        }
+    }
+
+    private IEnumerator DesaparecerMensaje(GameObject mensaje, float tiempo)
+    {
+        yield return new WaitForSeconds(tiempo);
+        Destroy(mensaje);
+    }
+}
